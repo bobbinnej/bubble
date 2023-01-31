f5e7066e150a90b7020c022a1c27cb94a28d1429
jackie <bobbinnej@gmail.com>
2023-01-31 17:52:09 +0300

    complete the jsx structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,29 @@
 import {Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
 import jobhuntLogo from './images/jobhuntLogo.png';
 import useStyles from './styles';
-
+import Posts from './components/PostsFolder/Posts';
+import Form from './components/FormFolder/Form';
 function App() {
   const classes = useStyles();
   return (
    <Container maxWidth="lg">
       <AppBar className={classes.appBar} position="absolute" color="inherit">
-        <Typography className={classes.title} align="center">Job Hunt Ls</Typography>
+        <Typography className={classes.title} variant="h4" align="center">Job Hunt Ls</Typography>
         <img className={classes.logo} src={jobhuntLogo} alt="job hunt ls" height="70"/>
       </AppBar>
+
+      <Grow in>
+        <Container>
+          <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+            <Grid item xs={12} sm={7}>
+              <Posts/>
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form/>
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
    </Container>
   );
 }
---
+++ client/src/components/PostsFolder/Posts.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Post from './Post/Post'
+
+function Posts() {
+  return (
+    <div>
+        <h1>posts</h1>
+        <Post/>
+        <Post/>
+    </div>
+  )
+}
+
+export default Posts
