55f6896270d61419b5220d0700a2ac2f1ccb8efb
jackie <bobbinnej@gmail.com>
2023-02-06 19:53:11 +0300

    eliminate cors error, display all posts array on the console

--- client/src/components/PostsFolder/Posts.js
+++ client/src/components/PostsFolder/Posts.js
@@ -1,7 +1,11 @@
-import React from 'react'
-import Post from './Post/Post'
+import React from 'react';
+import Post from './Post/Post';
+import {useSelector} from 'react-redux';
+
 
 function Posts() {
+  const postsReducer = useSelector((state)=>state.postsReducer);
+  console.log(postsReducer);
   return (
     <div>
         <h1>posts</h1>
